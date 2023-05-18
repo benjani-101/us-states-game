667b3ae37833821f0bd6d347b730b88bff13591b
benjani-101 <ben.david.crawley@gmail.com>
2023-05-18 17:22:13 +0100

    Version 2: added ability to give up and get remaining answers

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ playing = True
 score = 0
 correct_answers = []
 while playing:
-    answer_state = screen.textinput(title=f"Guess state: {score}/50", prompt="Guess the state:").title()
+    answer_state = screen.textinput(title=f"Guess state: {score}/50", prompt="Guess the state\n(or type exit for the "
+                                                                             "remaining answers):").title()
     if answer_state in df['state'].tolist() and answer_state not in correct_answers:
         row = df[df['state'] == answer_state]
         x = int(row['x'].iloc[0])
@@ -27,6 +28,18 @@ while playing:
         writer.write_state(answer_state, x, y)
         score += 1
         correct_answers.append(answer_state)
+        if score == 50:
+            playing = False
+    elif answer_state == 'Exit':
+        playing = False
+        writer.color('red')
+        for state in df['state'].tolist():
+            if state not in correct_answers:
+                row = df[df['state'] == state]
+                x = int(row['x'].iloc[0])
+                y = int(row['y'].iloc[0])
+                writer.write_state(state, x, y)
+
 
 
 
--- writer.py
+++ writer.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
-FONT = ("Verdana", 11, "normal")
+FONT = ("Verdana", 8, "normal")
 
 class Writer(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
         self.hideturtle()
+        self.speed(0)
 
     def write_state(self, state, x, y):
         self.goto(x, y)
